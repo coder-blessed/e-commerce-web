342f8d431aa9b1c29f8bbf490a521db7148b9ee7
Icom1blessed <isedebless8@gmail.com>
2024-10-10 14:24:20 +0100

    blessed-all-jkewfjwfn

--- e-commerce-new/src/components/Footer.jsx
+++ e-commerce-new/src/components/Footer.jsx
@@ -1,35 +1,28 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function Footer() {
   return (
     <>
-
-      <div id='footer-div' >
-
-
+      <div id="footer-div" className="bg-light w-100 h-100 mt-5 mb-5 position-relative">
         <div className="footer1">
-          <form action="" className='form111'>
-            <input type="text" placeholder="enter your email" />
+            <h1 className='hh'>subscribe newletter</h1>
+          <form action="" className="d-flex flex-column align-items-center mb-5">
+          <input type="email" placeholder="Enter your email" className="form-control mb-3" style={{ width: '20vw', heigh:'7%' }} />
 
-            <botton className="fbutton"><a href="#">SUBSCRIBE</a></botton>
+
+            <button className="btn btn-dark px-5 py-2 font-weight-bold" style={{width:'7vw'}}>
+              <a href="#" className="text-white text-decoration-none">SUBSCRIBE</a>
+            </button>
           </form>
-
         </div>
 
-        <div className="footer2">
-
-        </div>
-
-
-
+        <div className="footer2 bg-dark w-100 py-3"></div>
       </div>
-
-
-
     </>
-
-  )
+  );
 }
 
-export default Footer
+export default Footer;
